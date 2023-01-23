28abaa5908ed29e08bbdc43eeaf3b75020476493
B A B A K <babakir73@gmail.com>
2023-01-23 23:55:36 +0330

    missed imports ass to auth module

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { HttpClientModule } from '@angular/common/http';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -12,6 +15,8 @@ import { SignUpComponent } from './sign-up/sign-up.component';
     SignUpComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    HttpClientModule,
     CommonModule,
     AuthRoutingModule
   ]
